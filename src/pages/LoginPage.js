4fcad2b52efd4b85ff1f4ff5c662548f5517b3ee
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-07-29 21:44:10 +0900

    login page except axios

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../styles/pages/LoginPage.css';
+import LoginForm from '../components/LoginForm.js';
+import {useNavigate} from 'react-router-dom';
 
 const LoginPage = () => {
     return (
-        <div className='LoginPage'>
-            
-        </div>
+        <section className='LoginPage'>
+            <div style={{width: "100px", height: "20%"}}></div>
+            <div className='logo-text'>login</div>
+            <LoginForm/>
+            <div style={{width: "100px", height: "20%"}}></div>
+            <div id='none-auth-text'>아직 회원이 아니신가요 ?</div>
+            <button className='login-sigin-btn'>sign in</button>
+        </section>
     );
 };
 

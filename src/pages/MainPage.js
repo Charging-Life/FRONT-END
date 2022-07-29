68b83d287630b849368306805a82273216ed538a
GuHyeonWoo <ghw9174@gmail.com>
2022-07-29 19:07:27 +0900

    create kakaoMap

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../styles/pages/MainPage.css';
+import KakaoMap from '../components/KakaoMap';
+import Header from '../components/Header';
 
 const MainPage = () => {
     return (
         <div className='MainPage'>
-            
+            <Header/>
+            <KakaoMap/>
         </div>
     );
 };

de6b0e6a01dbf394f123fb1a5a36de878b85c385
GuHyeonWoo <ghw9174@gmail.com>
2022-07-29 21:44:47 +0900

    create header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,12 @@ import '../styles/components/Header.css';
 const Header = () => {
     return (
         <div className='Header'>
-            
+            {/* <div className='header_empty'/> */}
+            <img src='images/CL_Logo_horizontality.png' alt='logo' className='header_logo'/>
+            <div>
+                <img src='images/CL_icon_search.png' alt='search' className='header_search'/>
+                <img src='images/CL_icon_notice.png' alt='notice' className='header_notice'/>
+            </div>
         </div>
     );
 };

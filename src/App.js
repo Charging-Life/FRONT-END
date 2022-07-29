c944bca6cf1b945ea07c15f815e49054a69cc0ca
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-07-29 19:16:50 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import MainPage from './pages/MainPage.js';
+import LoginPage from './pages/LoginPage.js';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route exact path="/" element={<MainPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/*" element={<div>404</div>}/>
+      </Routes>
     </div>
   );
 }

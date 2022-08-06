e2f053e39b1f8075bfd75bc77251e5415a9b1087
GuHyeonWoo <ghw9174@gmail.com>
2022-08-06 20:23:09 +0900

    implement get chargerStatus translate

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/pages/MainPage.css';
 import KakaoMap from '../components/KakaoMap';
 import Header from '../components/Header';
+import axios from 'axios';
+import GetChargerStatusURL from '../security/GetChargerStatusURL.json'
 
 const MainPage = () => {
+    const url = GetChargerStatusURL['url'];
+    console.log(url);
+
+    useEffect(()=>{
+        axios.get(url).then(function(res){
+            console.log(res);
+        }).catch(function(err){
+            console.log(err);
+        })
+    },[])
+
     return (
         <div className='MainPage'>
             <div className='mainpage_header'>

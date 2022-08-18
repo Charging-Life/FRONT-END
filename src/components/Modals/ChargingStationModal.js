b114371e216053456f4470a38612738d152f7d70
GuHyeonWoo <ghw9174@gmail.com>
2022-08-18 16:36:53 +0900

    hold detail modal

--- src/components/Modals/ChargingStationModal.js
+++ src/components/Modals/ChargingStationModal.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import '../../styles/components/Modals/ChargingStationModal.css';
 
 const ChargingStationModal = ({show, onHide}) => {
-    
-    // 충전소 id : show[1] -> 통신해서 값 받아서 출력
+    const [stationDetail, setStationDetail] = useState({});
+    const [chargerList, setChargerList] = useState([]);
+
+    useEffect(()=>{
+        if(show[1]){
+            axios.get(`${process.env.REACT_APP_PROXY}/station/${show[1]}`)
+            .then((res)=>{
+                setStationDetail({
+                    stat: '',
+                    statNm: res.data.statNm,
+                    address: res.data.address,
+                    business: res.data.business,
+                    useTime: res.data.useTime,
+                    businessCall: res.data.businessCall,
+                    parkingFree: res.data.parkingFree
+                })
+                setChargerList(res.data.chargerDtos);
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+        }
+    },[show[1]])
 
     return (
         <div className='ChargingStationModal'>
@@ -12,22 +34,22 @@ const ChargingStationModal = ({show, onHide}) => {
                 <Modal.Header>
                     <div></div>
                     <span>충전소 운영 현황</span>
-                    <div id='close-icon' onClick={onHide}><img src='/images/CL_icon_close.png'/></div>
+                    <div id='close-icon' onClick={onHide}><img src='/images/icons/CL_icon_close.png'/></div>
                 </Modal.Header>
                 <Modal.Body>
                     <div className='modal-charging-info'>
                         <span id='charging-state'>충전 가능</span>
-                        <div id='charging-location'>옥계현진에버빌엠파이어</div>
-                        <div id='charging-location-info'>경상북도 구미시 산호대로 21길 6</div>
+                        <div id='charging-location'>{stationDetail.statNm}</div>
+                        <div id='charging-location-info'>{stationDetail.address}</div>
                         <br/>
                         <div id='charging-type'><b>충전기 타입</b> &nbsp; DC콤보</div>
                         <hr/>
                         <div id='charging-station-info-text'><b>충전소 정보</b> </div>
                         <div className='charging-station-info'>
-                            <div>운영 기관 &nbsp;&nbsp; 지커넥트</div>
-                            <div>운영 시간 &nbsp;&nbsp; 24시간 이용가능</div>
-                            <div>연락처 &nbsp;&nbsp; 1544-4279</div>
-                            <div>충전요금 &nbsp;&nbsp; 유료</div>
+                            <div>운영 기관 &nbsp;&nbsp; {stationDetail.business}</div>
+                            <div>운영 시간 &nbsp;&nbsp; {stationDetail.useTime?stationDetail.useTime:'-'}</div>
+                            <div>연락처 &nbsp;&nbsp; {(stationDetail.businessCall&&stationDetail.businessCall!=='null')?stationDetail.businessCall:'-'}</div>
+                            <div>주차 요금 &nbsp;&nbsp; {stationDetail.parkingFree?'무료':'유료'}</div>
                         </div>
                         <br/><br/>
                     </div>

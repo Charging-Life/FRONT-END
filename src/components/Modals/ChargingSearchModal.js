602264eb2523cef8b4b5462be2e37d354ad3d7db
GuHyeonWoo <ghw9174@gmail.com>
2022-08-21 01:32:40 +0900

    implement searchModal

--- src/components/Modals/ChargingSearchModal.js
+++ src/components/Modals/ChargingSearchModal.js
@@ -14,27 +14,48 @@ const ChargingSearchModal = ({show, onHide}) => {
         setSearchValue(e.target.value);
     }
 
-    const clickCharging = (id) =>{
-        if(selectCharging.includes(id)){
-            for(let i = 0; i < selectCharging.length; i++){
-                if(selectCharging[i] === id){
-                    setSelectCharging(selectCharging.splice(i, 1));
-                    i--;
-                }
-            }
+    useEffect(()=>{
+        setSearchValue('');
+        setChargingList('');
+        setSelectCharging([]);
+    },[onHide])
+
+    const clickCharging = (charging) =>{
+        let add = -1;
+        selectCharging.map((charge, i) => {
+            if(charge['statId'] === charging['statId']) add=i;
+        })
+
+        if(add === -1){
+            setSelectCharging([...selectCharging, {statNm: charging['statNm'],statId: charging['statId']}])
         }
         else{
-            setSelectCharging([...selectCharging, id]);
+            const temp = [];
+            selectCharging.map((charge) => {
+                if(charge['statId'] !== charging['statId']) temp.push(charge);
+            })
+            setSelectCharging(temp);
         }
+    }
+    const checkClassName = (id) =>{
+        let className = 'search_click'
+        selectCharging.map(charge => {
+            if(charge['statId'] === id){
+                className = 'search_clicked';
+                return;
+            } 
+        })
+        return className;
     }
 
     const showChargingList = () => {
+        console.log(chargingList);
         const showList = [];
 
-        if(chargingList !== []){
+        if(chargingList.length > 0){
             for(let i = 0; i < chargingList.length; i++){
                 showList.push(
-                    <div onClick={()=>{clickCharging(chargingList[i]['statId'])}} className={selectCharging.includes(chargingList[i]['statId'])?'search_clicked':'search_click'}>
+                    <div onClick={()=>{clickCharging(chargingList[i])}} className={checkClassName(chargingList[i]['statId'])}>
                         <div>
                             {chargingList[i]['statNm']}
                         </div>
@@ -46,8 +67,9 @@ const ChargingSearchModal = ({show, onHide}) => {
             }
         }
         else{
-            showList.push(<div>검색된 충전소가 없습니다.</div>)
+            showList.push(<div className='search_empty2'>검색 결과가 없습니다.</div>);
         }
+
 
         return(showList);
     }
@@ -57,49 +79,76 @@ const ChargingSearchModal = ({show, onHide}) => {
             axios.get(`${process.env.REACT_APP_PROXY}/station?statNm=${searchValue}`)
             .then((res)=>{
                 setChargingList(res.data);
-                console.log(res);
             })
             .catch((err)=>{
                 console.log(err);
             })
         }
+        else{
+            setChargingList([]);
+        }
     }, [searchValue])
 
     const saveSelected = ()=>{
-        if(selectCharging === []){
-            inputRef.current.focus();
+        if(selectCharging.length > 0){
+            axios.post(`${process.env.REACT_APP_PROXY}/member/station`,{
+                statId: selectCharging
+            },{
+                headers: {Authorization: localStorage.getItem('CL_accessToken')}
+            })
+            .then((res)=>{
+                console.log(res);
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+
             return;
         }
-        axios.post(`${process.env.REACT_APP_PROXY}/member/station`,{
-            statId: selectCharging
-        },{
-            headers: {Authorization: localStorage.getItem('CL_accessToken')}
+        inputRef.current.focus();
+    }
+
+    const delCharging = (id) =>{
+        const temp = [];
+        selectCharging.map((charge) => {
+            if(charge['statId'] !== id) temp.push(charge);
         })
-        .then((res)=>{
-            console.log(res);
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
+        setSelectCharging(temp);
+    }
+    const showSelectHorizen = () =>{
+        const showList = [];
+
+        if(selectCharging !== []){
+            for(let i = selectCharging.length-1; i >= 0 ; i--){
+                showList.push(
+                    <div>
+                        <div>{selectCharging[i]['statNm']}</div>
+                        <button onClick={()=>{delCharging(selectCharging[i]['statId'])}}><img alt='취소' src='images/icons/CL_icon_close.png'/></button>
+                    </div>
+                );
+            }
+        }
+
+        return showList;
     }
 
     return (
         <div className='ChargingSearchModal'>
             <Modal centered show={show} onHide={onHide}>
-                <Modal.Header>
-                    <div className='search_header'>
-                        <input ref={inputRef} placeholder='구미 시청' value={searchValue} onChange={changeInput}/>
-                        <img/>
-                    </div>
+                <Modal.Header className='search_header'>
+                    <input ref={inputRef} placeholder='검색' value={searchValue} onChange={changeInput}/>
+                    <img alt='검색' src='images/icons/CL_icon_search.png'/>
                 </Modal.Header>
+                {selectCharging.length>0&&
+                <div className='search_select_horizenList'> 
+                    {showSelectHorizen()}
+                </div>}
                 <Modal.Body>
                     <div className='search_body'>
-                        {searchValue==='' && chargingList === ''
-                        ?<div>충전소를 검색해보세요.</div>
-                        :showChargingList()}
+                        {searchValue==='' && chargingList === ''?<div className='search_empty'>충전소를 검색해보세요.</div>:showChargingList()}
                     </div>
                     <div className='search_footer'>
-                        <div onClick={onHide()}>닫기</div><div onClick={()=>{saveSelected()}}>등록</div>
+                        <div onClick={onHide}>닫기</div><div onClick={()=>{saveSelected()}}>등록</div>
                     </div>
                 </Modal.Body>
             </Modal>

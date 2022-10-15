9c56c83c74490dd0b6dfe83f0b9f3a709df27b99
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-10-15 21:33:59 +0900

    modify main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,57 +1,20 @@
-import React, { useEffect, useState } from 'react';
 import '../styles/pages/MainPage.css';
-import KakaoMap from '../components/KakaoMap';
+import Manager_main from '../components/Manager_main';
+import User_main from '../components/User_main';
 import Header from '../components/Header';
-import axios from 'axios';
+import Bottombar from '../components/Bottombar';
 
 const MainPage = () => {
-    const [station, setStation] = useState([]);
-    const [stationState, setStationState] = useState([]);
-    const [location, setLocation] = useState('');
 
-    useEffect(()=>{
-        axios.get(`${process.env.REACT_APP_PROXY}/station/manager`,{
-            headers: {
-                Authorization: localStorage.getItem('CL_accessToken')
-            }
-        })
-        .then((res)=>{
-            setStation(res.data);
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-        if(location !== ''){
-            axios.get(`${process.env.REACT_APP_PROXY}/station/manager`,{
-                headers: {
-                    Authorization: localStorage.getItem('CL_accessToken')
-                }
-            })
-            .then((res)=>{
-                setStation(res.data);
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
-
-            axios.get(`${process.env.REACT_APP_GET_STATION_URL}`)
-            .then((res)=>{
-                setStationState(res.data.items.item);
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
-        }
-    },[location])
+    const isManager = localStorage.getItem('CL_auth') && localStorage.getItem('CL_auth') === 'MANAGER' ? true : false;
 
     return (
         <div className='MainPage'>
             <div className='mainpage_header'>
                 <Header page={"main"}/>
-            </div>
-            <div className='mainpage_kakaomap'>
-                <KakaoMap station={station} stationState={stationState} setLocation={setLocation}/>
-            </div>
+            </div> 
+            { isManager ? <Manager_main/> : <User_main/> }
+            { isManager ? '' : <Bottombar value={1}/> }
         </div>
     );
 };

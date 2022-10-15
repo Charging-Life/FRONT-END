069447cc97a31c0f1b7b285a8f836b79c0ca424b
GuHyeonWoo <ghw9174@gmail.com>
2022-10-16 03:56:19 +0900

    fix route

--- src/components/bars/Bottombar.js
+++ src/components/bars/Bottombar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { useNavigate } from 'react-router';
 
-import './/..//styles/components/Bottombar.css';
+import './../../styles/components/bars/Bottombar.css';
 
 const Bottombar = ({value}) => {
 

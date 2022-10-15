cfb630834507f61ffd1a5cf06ff0a538da8dcb68
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-10-15 21:14:34 +0900

    modify bookmark icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router';
 // import axios from 'axios';
 // import useInterval from '../components/useInterval';
 
-const Header = ({page}) => {
+const Header = ({page, isListView, setIsListView}) => {
     const [showNotice, setShowNotice] = useState(false);
     const [showFilter, setShowFilter] = useState(false);
     const [isUser, setIsUser] = useState(false);
@@ -23,43 +23,20 @@ const Header = ({page}) => {
         }
     }, []);
 
-    // useInterval(() => {
-    //     // 반복할 로직 : 알림 쌓인것 있는지 1분마다 요청
-    //     axios.get(`${PROXY}/alarm`, {
-    //         headers : {
-    //             Authorization: localStorage.getItem('CL_accessToken')
-    //         }
-    //     })
-    //     .then((res) => {
-    //         // 받아오는 데이터 중에 안읽은 것이 있으면 이미지 변경
-    //         console.log(res);
-    //     })
-    //     .catch((err) => {
-    //         console.log(err);
-    //     });
-
-    // }, 60000); 
-
     return (
         <>
             <div className='Header'>
                 <img src='images/CL_Logo_horizontality.png' alt='logo' 
                     className='header_logo' onClick={onClickLogo}/>
                 {
-                    page === "main" &&
+                    page === "main" ?
                     <div>
-                        <img src='images/icons/CL_icon_search.png' alt='search' 
-                            className='header_search'
-                            onClick={()=>{setShowFilter(true)}}/>
                         {
                             isUser ? 
                             <>
                                 <img src='images/icons/CL_icon_notice.png' alt='notice' 
                                     className='header_notice'
                                     onClick={()=>{setShowNotice(true)}}/>
-                                <img src='images/icons/CL_icon_setting.png' alt='setting' 
-                                    className='header_setting'
-                                    onClick={()=>{navigate('/my')}}/>
                             </> : 
                             <>
                                 <img src='images/icons/CL_icon_login.png' alt='login' 
@@ -70,7 +47,25 @@ const Header = ({page}) => {
                                     onClick={()=>{navigate('/signup')}}/>
                             </>
                         }
-                    </div> 
+                    </div> :
+                    page === "bookmark" ? 
+                    <div>
+                        { isListView ? 
+                            <img src={'images/icons/CL_icon_map_view.png'} alt='view' 
+                            className='header_view map'
+                            onClick={()=>{setIsListView(!isListView)}}/>
+                            :
+                            <>
+                            <img src='images/icons/CL_icon_search.png' alt='search' 
+                            className='header_search'
+                            onClick={()=>{setShowFilter(true)}}/>
+                            <img src={'images/icons/CL_icon_list_view.png'} alt='view' 
+                            className='header_view'
+                            onClick={()=>{setIsListView(!isListView)}} /> </>}
+                        <img src='images/icons/CL_icon_notice.png' alt='notice' 
+                                    className='header_notice'
+                                    onClick={()=>{setShowNotice(true)}}/>
+                    </div> : ''
                 }
             </div>
 

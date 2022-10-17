9ead59e64835bc95286720abd6d88a210a564ca2
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-10-17 23:33:09 +0900

    add Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MainPage from './pages/MainPage.js';
 import LoginPage from './pages/LoginPage.js';
 import SignupPage from './pages/SignupPage.js';
 import MyPage from './pages/MyPage.js';
+import BoardPage from './pages/BoardPage.js';
 import BookmarkPage from './pages/BookmarkPage';
 import {Routes, Route} from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
@@ -21,6 +22,7 @@ function App() {
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/signup" element={<SignupPage/>}/>
         <Route path="/bookmark" element={<BookmarkPage/>}/>
+        <Route path="/board" element={<BoardPage/>}/>
         <Route path="/my" element={<MyPage/>}/>
         <Route path="*" element={<div>404</div>}/>
       </Routes>

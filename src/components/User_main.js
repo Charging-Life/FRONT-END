dc612c1a382b4fc0e0128f9c32cb466ef59fce46
GuHyeonWoo <ghw9174@gmail.com>
2022-10-18 03:13:20 +0900

    reflected review & fixed text

--- src/components/User_main.js
+++ src/components/User_main.js
@@ -4,58 +4,78 @@ import axios from 'axios';
 import ProgressBar from './bars/ProgressBar';
 import { useState } from 'react';
 import useInterval from '../hooks/useInterval';
+import QRModal from './Modals/QRModal';
 
 const User_main = () => {
+    // qr코드를 생성하는 모달
+    const [qrModal, setQRModal] = useState(false);
+    // 시작 시간을 저장하는 useState
     const [startTime, setStartTime] = useState({
-        sHour: '22',
-        sMinute: '10',
+        sHour: '14',
+        sMinute: '30',
         status: 'SLOW',
         change: true
     });
+    // 시작 시간을 set 하는 함수 -> 후에 axios로 수정 필요
     const setSTime = () => {
+        // qr코드를 생성하는 모달이 켜져있는 동안은 실행 x
+        // -> 주기적으로 실행되는데 qr 모달의 부모 요소기 때문에 재렌더링 되기 때문
+        if(qrModal) return;
         setStartTime({
-            sHour: '22',
-            sMinute: '10',
+            sHour: '14',
+            sMinute: '30',
             status: 'SLOW',
+            // state가 바뀐다는 것을 명시하기 위한 프로퍼티
             change: !startTime.change
         })
-        console.log(startTime);
     }
+    // 1분마다 함수를 실행하는 hook
     useInterval(setSTime ,60000);
 
+    // 진행된 시간을 저장하는 useState
     const [proceedTime, setProceedTime] = useState({
         pHour: '',
         pMinute: ''
     });
 
+    // 얼마나 진행됐는지 계산하는 함수
     const proceededTime = () => {
         const today = new Date();
-
+        // 현재 시간과 분
         let currentHour = today.getHours();
         const currentMinute = today.getMinutes();
-
+        // 시작한 시간과 분
         const startHour = parseInt(startTime.sHour);
         const startMinute = parseInt(startTime.sMinute);
 
+        // 시작한 시간이 현재 시간보다 클 때
         if (startHour > currentHour) {
+            // 현재 시간에 24를 더해줌으로서 예외처리
             currentHour = today.getHours() + 24;
         }
 
+        // 현재 시간의 총 분
         const sumCurrent = currentHour * 60 + currentMinute;
+        // 시작 시간의 총 분
         const sumStart = startHour * 60 + startMinute;
+        // 진행 된 시간의 총 분
         const proceeded = sumCurrent - sumStart;
 
+        // 진행 된 시와 분을 저장
         setProceedTime({
             pHour: parseInt(proceeded / 60),
             pMinute: proceeded % 60
         })
     }
 
+    // 시작 시간을 받아올 때마다 진행 시간 계산
     useEffect(()=>{
         proceededTime();
     },[startTime])
 
     useEffect(() => {
+        // 사용자의 정보 받아오는 axios
+        // 후에 데이터 처리 필요
         const auth = localStorage.getItem('CL_auth') && localStorage.getItem('CL_auth').toLowerCase();
         axios.get(`${process.env.REACT_APP_PROXY}/member/${auth}`, {
             headers: {
@@ -75,8 +95,9 @@ const User_main = () => {
             <div id='User_main'>
                 <div id='main_progress_box'>
                     <div id='main_progress_info'>
-                        <div>{'이수화'}님</div>
-                        <div>현재 {proceedTime.pHour !== 0 && proceedTime.pHour + '시'} {proceedTime.pMinute}분 째
+                        {/* 후에 닉네임 처리 */}
+                        <div>{'이수화'}님</div> 
+                        <div>현재 {proceedTime.pHour !== 0 && proceedTime.pHour + '시간'} {proceedTime.pMinute}분 째
                             <span>
                                 {startTime.status === 'FAST' ? ' 급속 충전 ' : ' 완속 충전'} 중
                             </span> 입니다.
@@ -94,12 +115,17 @@ const User_main = () => {
                         <div>충전소 검색</div>
                         <img src='images/icons/CL_icon_loc_white.png' alt='loc' />
                     </div>
-                    <div id='main_qr_box'>
+                    <div id='main_qr_box' onClick={()=>setQRModal(true)}>
                         <div>충전 시작하기</div>
                         <img src='images/icons/CL_icon_QR.png' />
                     </div>
                 </div>
             </div>
+
+            <QRModal
+                show={qrModal}
+                onHide={()=>setQRModal(false)}
+            />
         </>
     );
 };
--- src/components/bars/ProgressBar.js
+++ src/components/bars/ProgressBar.js
@@ -2,40 +2,54 @@ import React, { useEffect, useState } from 'react';
 import '../../styles/components/bars/ProgressBar.css';
 
 const ProgressBar = ({ startTime }) => {
-    const today = new Date();
-
+    // 끝나는 시간 출력 예외 처리
     const checkHour = () => {
         const shour = parseInt(startTime.sHour);
 
+        // FAST면 시작시간을 기준으로 한시간 뒤의 경우를 계산
         if (startTime.status === 'FAST') {
             if (shour + 1 >= 24) return shour - 23;
             return shour + 1;
         }
+        // SLOW면 시작시간을 기준으로 14시간 뒤의 경우를 계산
         else {
             if (shour + 14 >= 24) return shour - 10;
             return shour + 14;
         }
     }
+    // 남는 시간을 계산하는 함수
     const checkLeftTime = () => {
+        const today = new Date();
+
+        // 현재 시와 분
         const currentHour = today.getHours();
         const currentMinute = today.getMinutes();
+
+        // 끝나는 시간
         let endHour = endTime.eHour;
 
+        // 현재 시간이 끝나는 시간보다 크다면 끝나는 시간에 24를 더하여 예외처리
         if (currentHour > endTime.eHour) {
             endHour = endTime.eHour + 24;
         }
 
+        // 현재시간의 총 분, 끝나는 시간의 총 분
         const sumCurrentTime = currentHour * 60 + currentMinute;
         const sumEndTime = endHour * 60 + endTime.eMinute;
 
+        // 남는 시간의 총 분
         const leftVal = sumEndTime - sumCurrentTime;
 
+        // 남는 시간의 시와 분
         const leftHour = parseInt(leftVal / 60);
         const leftMinute = leftVal % 60;
 
+        // 퍼센트를 계산하기 위해 SLOW, FAST를 구분하여 분모를 결정
         const standard = startTime.status === 'FAST' ? 60 : 14 * 60;
+        // 진행 퍼센트 계산
         const tPercent = 100 - (100 * leftVal / standard);
 
+        // 남는 시간 정리
         setLeftTime({
             lHour: leftHour,
             lMinute: leftMinute,
@@ -44,10 +58,12 @@ const ProgressBar = ({ startTime }) => {
         })
     }
 
+    // 끝나는 시간을 저장하는 useState
     const [endTime, setEndTime] = useState({
         eHour: checkHour(),
         eMinute: parseInt(startTime.sMinute)
     });
+    // 남는 시간을 저장하는 useState
     const [leftTime, setLeftTime] = useState({
         lHour: '',
         lMinute: '',
@@ -55,6 +71,7 @@ const ProgressBar = ({ startTime }) => {
         pxPercent: ''
     });
 
+    // 시작시간이 최신화 될 때마다 남은 시간을 계산
     useEffect(() => {
         checkLeftTime();
     }, [startTime, endTime]);
@@ -76,7 +93,7 @@ const ProgressBar = ({ startTime }) => {
             </div>
             <div id='progress_bar_bottom'>
                 {`${startTime.sHour}:${startTime.sMinute}`}
-                <div>{leftTime.lHour !== 0 && leftTime.lHour + '시'} {leftTime.lMinute}분 남았습니다.</div>
+                <div>{leftTime.lHour !== 0 && leftTime.lHour + '시간'} {leftTime.lMinute}분 남았습니다.</div>
                 {`${endTime.eHour}:${endTime.eMinute}`}
             </div>
         </>
